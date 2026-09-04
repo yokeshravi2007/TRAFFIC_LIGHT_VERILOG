48d86e643654c2a9a501ea5419f3b64c6fec2347
YOKESH R <yokeshravi2007@gmail.com>
2026-09-04 23:59:47 +0530

    Add Verilog source and testbench

---
+++ traffic_light_tb.v
@@ -0,0 +1,41 @@
+`timescale 1ns / 1ps
+//YOKESH R
+
+module traffic_light_tb_v;
+
+	// Inputs
+	reg clk;
+	reg reset;
+
+	// Outputs
+	wire red;
+	wire green;
+	wire yellow;
+
+	// Instantiate the Unit Under Test (UUT)
+	traffic_light uut (
+		.clk(clk), 
+		.reset(reset), 
+		.red(red), 
+		.green(green), 
+		.yellow(yellow)
+	);
+  always #5 clk =~clk;
+	initial begin
+	clk = 0;
+    reset = 1;
+
+    #10;
+    reset = 0;
+
+    #10;
+    #10;
+    #10;
+    #10;
+    #10;
+	 $finish;
+ 
+	end
+      
+endmodule
+
